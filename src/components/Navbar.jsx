c2425ae173bdb97bf263eb3c157537e7ea939666
HorusMabia <horus.mabia@gmail.com>
2023-02-05 22:44:58 +0100

    react component navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <div className= "">
+            <i>H</i>
+            <i>Trophies</i>
+            <i>Gallery</i>
+            <i>Contact</i>
+        </div>
+
+    );
+};
